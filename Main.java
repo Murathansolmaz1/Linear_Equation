dca88beabe5a0c34824efe9174f8e3658d5c7643
Murathan Solmaz <murathansolmaz555@gmail.com>
2021-01-25 15:57:50 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,89 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a,b,c,d,e,f: ");
+
+        double a,b,c,d,e,f;
+
+        a = input.nextDouble();
+        b = input.nextDouble();
+        c = input.nextDouble();
+        d = input.nextDouble();
+        e = input.nextDouble();
+        f = input.nextDouble();
+
+        LinearEquation Linear = new LinearEquation(a,b,c,d,e,f);
+
+        if (Linear.isSolvable()){
+            System.out.println("X is" + Linear.getX() + " Y is " + Linear.getY());
+        }
+        else{
+            System.out.println("The equation has no solution.");
+        }
+    }
+}
+
+class LinearEquation{
+    private double a,b,c,d,e,f;
+
+    LinearEquation(){
+        this.a=a;
+        this.b=b;
+        this.c=c;
+        this.d=d;
+        this.e=e;
+        this.f=f;
+    }
+
+    LinearEquation(double a, double b,double c, double d, double e, double f){
+        this.a=a;
+        this.b=b;
+        this.c=c;
+        this.d=d;
+        this.e=e;
+        this.f=f;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public double getD() {
+        return d;
+    }
+
+    public double getE() {
+        return e;
+    }
+
+    public double getF() {
+        return f;
+    }
+
+    public boolean isSolvable(){
+        return ((a*d-b*c)!=0);
+    }
+    public double getX(){
+        return (e*d-b*f)/(a*d-b*c);
+    }
+
+    public double getY(){
+        return (a*f-e*c)/(a*d-b*c);
+    }
+
+}
